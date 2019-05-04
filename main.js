83689e848e7a9a4bc0c32bc9f3fba6949c29ed8f
Jamil Mbabaali <jamilnineteen91@gmail.com>
2019-05-03 21:42:27 -0500

    created new repo & copied existing files into it

---
+++ main.js
@@ -0,0 +1,16 @@
+var circle = document.getElementById("audio-player");
+var ctx = circle.getContext("2d");
+ctx.lineWidth=5;
+ctx.fillstyle='#D3D3D3';
+ctx.beginPath();
+ctx.arc(100, 75, 50, 1, 3 * Math.PI);
+ctx.stroke();
+
+var seek = document.querySelector(".seek-bar");
+var ctx = seek.getContext("2d");
+ctx.fillstyle="#0000FF";
+ctx.lineWidth=5;
+ctx.beginPath();
+ctx.arc(100, 75, 50, 1, .5 * Math.PI);
+ctx.fill()
+ctx.stroke();
