08413bb27639a28f24b289dac6e11aa7e61938a2
Jamil Mbabaali <jamilnineteen91@gmail.com>
2019-05-13 18:56:39 -0500

    edited

---
+++ main.js
@@ -0,0 +1,17 @@
+const app = ()=>{
+    const song = document.querySelector('.song');
+    const play =document.querySelector('.play');
+    const outline = document.querySelector('.moving-outline circle');
+
+    // Songs
+    
+    // Length of outline
+    const outlineLength = outline.getTotalLength();
+    
+    //Duration
+    let duration = 
+}
+
+
+
+app();
