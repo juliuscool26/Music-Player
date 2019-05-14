fbe1fafff6332587312fa867def08a53e056e2a2
Jamil Mbabaali <jamilnineteen91@gmail.com>
2019-05-13 23:22:58 -0500

    edited click event on cover art img

--- main.js
+++ main.js
@@ -7,9 +7,33 @@ const app = ()=>{
     
     // Length of outline
     const outlineLength = outline.getTotalLength();
-    
+
     //Duration
-    let duration = 
+    let songDuration = song.duration;
+    console.log(songDuration)
+
+    outline.style.strokeDasharray = outlineLength;
+    outline.style.strokeDashoffset=400;
+
+    //Play music
+    play.addEventListener('click',() => {
+        if (song.paused){
+            song.play();
+        }else{
+            song.pause();
+        }
+        
+    });
+    
+    song.ontimeupdate = () => {
+        let currentTime=song.currentTime;
+
+    }
+
+    // Progress bar
+    let progress = outlineLength;
+    
+
 }
 
 
